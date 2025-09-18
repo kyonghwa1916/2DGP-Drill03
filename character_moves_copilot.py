9db86bbeee6ac7a9b088efea9db966e8a235a871
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-18 23:19:25 +0900

    second command
    배치된 화면이 보이지 않아. canvas가 보고 화면을 인지할 수 있을정도의 딜레이를 지정해줘.

--- character_moves_copilot.py
+++ character_moves_copilot.py
@@ -12,7 +12,7 @@ while pico2d.running:
     pico2d.clear_canvas()
     draw_scene()
     pico2d.update_canvas()
-    pico2d.delay(0.01)
+    pico2d.delay(0.5)  # 딜레이를 0.5초로 늘려서 화면이 보이도록 함
     for event in pico2d.get_events():
         if event.type == pico2d.SDL_QUIT:
             pico2d.running = False
