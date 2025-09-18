df789d0c1a2d3cb30071a6725551dfffc8687acf
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-18 23:46:59 +0900

    def func
    함수 선언. 내용 없음.

--- character_moves.py
+++ character_moves.py
@@ -5,8 +5,21 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
-delay(5)
+def SquareMove():
+    pass
+
+def TriangleMove():
+    pass
+
+def CircleMove():
+    pass
+
+while True:
+    SquareMove()
+    TriangleMove()
+    CircleMove()
+    grass.draw_now(400, 30)
+    character.draw_now(400, 90)
+    delay(5)
 
 close_canvas()
