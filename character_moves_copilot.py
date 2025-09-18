d4611c4539414fd5b9764131c90b0a1c920e3586
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-18 23:17:30 +0900

    first command
    폴더 내의 이미지들을 이용해서 grass는 (800*800)화면의 바닥에, character는 화면의 중앙 하단에 배치해줘.

---
+++ character_moves_copilot.py
@@ -0,0 +1,20 @@
+import pico2d
+
+pico2d.open_canvas(800, 800)
+grass = pico2d.load_image('grass.png')
+character = pico2d.load_image('character.png')
+
+def draw_scene():
+    grass.draw(400, 40)  # 바닥 중앙에 배치 (grass 높이 80 기준)
+    character.draw(400, 90)  # 화면 중앙 하단에 배치 (character 높이 90 기준)
+
+while pico2d.running:
+    pico2d.clear_canvas()
+    draw_scene()
+    pico2d.update_canvas()
+    pico2d.delay(0.01)
+    for event in pico2d.get_events():
+        if event.type == pico2d.SDL_QUIT:
+            pico2d.running = False
+
+pico2d.close_canvas()
